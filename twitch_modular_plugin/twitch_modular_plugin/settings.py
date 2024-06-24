320f4438c1598cd18416ab67d9e18696496b4e29
pr0d1gypumkn <akeshta@ualberta.ca>
2024-06-24 02:03:27 -0600

    added deployed machine to allowed hosts

--- twitch_modular_plugin/twitch_modular_plugin/settings.py
+++ twitch_modular_plugin/twitch_modular_plugin/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)bf+4lps(id=^j0qtp74)jii1om(!u4&!zkcr!v7p$znavix9f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'twitch-ms-backend-f3781f9ffed7.herokuapp.com']
 
 
 # Application definition
