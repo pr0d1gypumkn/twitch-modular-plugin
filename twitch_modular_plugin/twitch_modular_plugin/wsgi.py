b3434267ff41426270ce69bb11ca7a62a6b2feb6
pr0d1gypumkn <akeshta@ualberta.ca>
2024-06-24 01:52:24 -0600

    Updating imports for deployment

--- twitch_modular_plugin/twitch_modular_plugin/wsgi.py
+++ twitch_modular_plugin/twitch_modular_plugin/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitch_modular_plugin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitch_modular_plugin.twitch_modular_plugin.settings')
 
 application = get_wsgi_application()
